22e8778f05dae66cc535fefbe7e57f1c8df95228
Devin Walters <devinw@gmail.com>
2014-06-12 13:48:51 -0500

    Initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject cloclogo "0.1.0"
+  :description "Indiana Jones and the Crystal Skull, except Clojure"
+  :url "https://github.com/devn/cloclogo"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.analyzer "0.2.0"]])
---
+++ src/cloclogo/core.clj
@@ -0,0 +1,6 @@
+(ns cloclogo.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
